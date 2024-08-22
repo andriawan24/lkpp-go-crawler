6110da9c0718889fd37f2f50f380e52b911ef36d
Naufal Fawwaz <fawaznaufal23@gmail.com>
2024-08-22 22:53:30 +0700

    feature: initial commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"lexicon/lkpp-go-crawler/models"
+	"log"
+	"os"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+	var products []models.Product
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Requesting to visit", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Error", err)
+	})
+
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		product := models.Product{}
+
+		product.Url = e.ChildAttr("a", "href")
+		product.Name = e.ChildText(".product-name")
+		product.Price = e.ChildText(".price")
+		product.Image = e.ChildAttr("img", "src")
+
+		products = append(products, product)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(r.Request.URL, "scraped!")
+
+		file, err := os.Create("products.csv")
+		if err != nil {
+			log.Fatalln("Failed to create output CSV", err)
+		}
+
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+
+		headers := []string{
+			"Url",
+			"Image",
+			"Name",
+			"Price",
+		}
+		writer.Write(headers)
+
+		for _, product := range products {
+			record := []string{
+				product.Url,
+				product.Image,
+				product.Name,
+				product.Price,
+			}
+			writer.Write(record)
+		}
+
+		defer writer.Flush()
+	})
+
+	c.Visit("https://www.scrapingcourse.com/ecommerce")
+}
---
+++ models/products.go
@@ -0,0 +1,5 @@
+package models
+
+type Product struct {
+	Url, Name, Image, Price string
+}
