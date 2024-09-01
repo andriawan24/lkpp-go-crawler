611be057510ffde4ef1e61c82344dbfc2321e6f7
Naufal Fawwaz <fawaznaufal23@gmail.com>
2024-09-01 19:41:40 +0700

    feature: add rod to implement headless scraping using chrome tools

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -7,44 +7,57 @@ import (
 	"lexicon/lkpp-go-crawler/common"
 	"lexicon/lkpp-go-crawler/crawler/services"
 	"lexicon/lkpp-go-crawler/scraper/models"
+	"os"
+	"strings"
 	"time"
 
+	crawler_model "lexicon/lkpp-go-crawler/crawler/models"
+	scraper_service "lexicon/lkpp-go-crawler/scraper/services"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/go-rod/rod"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/queue"
 	"gopkg.in/guregu/null.v4"
 )
 
 func StartScraper() error {
-	unscrapedUrls, err := services.GetUnscrapedUrl()
+	unscraped_urls, err := services.GetUnscrapedUrl()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("[started]: Unscraped urls", len(unscrapedUrls))
+	fmt.Println("[started]: Unscraped urls", len(unscraped_urls))
 
-	queue, err := queue.New(2, &queue.InMemoryQueueStorage{MaxSize: 10000})
-	if err != nil {
-		return err
-	}
-
-	scraper, err := buildScraper(queue)
-	if err != nil {
-		return err
-	}
-
-	for _, url := range unscrapedUrls {
-		queue.AddURL(url.Url)
-	}
-
-	queue.Run(scraper)
-	scraper.Wait()
+	url := unscraped_urls[0].Url
+	page := rod.New().MustConnect().MustPage(url)
+	page.MustWaitStable().MustScreenshot("test.png")
 
 	return nil
+
+	// queue, err := queue.New(2, &queue.InMemoryQueueStorage{MaxSize: 10000})
+	// if err != nil {
+	// 	return err
+	// }
+
+	// scraper, err := buildScraper(queue)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// for _, url := range unscraped_urls {
+	// 	queue.AddURL(url.Url)
+	// }
+
+	// queue.Run(scraper)
+
+	// scraper.Wait()
+
+	// return nil
 }
 
 func buildScraper(queue *queue.Queue) (*colly.Collector, error) {
 	var currentUrl string
-	injunctions := []models.Injunction{}
 
 	newExtraction := models.Extraction{
 		Metadata: models.Metadata{},
@@ -65,43 +78,95 @@ func buildScraper(queue *queue.Queue) (*colly.Collector, error) {
 
 	c.SetRequestTimeout(time.Minute * 2)
 
-	c.OnHTML("input", func(h *colly.HTMLElement) {
-		fmt.Println("Data ke ", currentUrl, h.Attr("value"))
+	c.OnHTML("table.definition > tbody > tr", func(h *colly.HTMLElement) {
+		if !strings.Contains(currentUrl, "non-aktif") {
+			text := strings.TrimSpace(h.ChildText("td:nth-child(2)"))
+			switch h.Index {
+			case 0:
+				id := sha256.Sum256([]byte(text))
+				newExtraction.Id = hex.EncodeToString(id[:])
+				newExtraction.Metadata.ID = hex.EncodeToString(id[:])
+				newExtraction.Metadata.Title = text
+			case 1:
+				newExtraction.Metadata.NPWP = text
+			case 2:
+				newExtraction.Metadata.Address = text
+			case 3:
+				newExtraction.Metadata.City = text
+			case 4:
+				newExtraction.Metadata.Province = text
+			}
+		}
 	})
 
-	c.OnHTML("textarea", func(h *colly.HTMLElement) {
-		fmt.Println("Data ke ", currentUrl, h.Text)
+	c.OnHTML("table.table-list > tbody > tr:nth-child(1) > td:nth-child(1)", func(h *colly.HTMLElement) {
+		if !strings.Contains(currentUrl, "non-aktif") {
+			h.DOM.Contents().Each(func(i int, s *goquery.Selection) {
+				if i == 0 {
+					cleanedText := strings.TrimSpace(s.Text())
+					newExtraction.Metadata.Number = cleanedText
+				} else if i == 1 {
+					rule := strings.TrimSpace(s.Find(".header").Text())
+					description := strings.TrimSpace(s.Find(".description").Text())
+					newExtraction.Metadata.Rule = rule
+					newExtraction.Metadata.Description = description
+				}
+			})
+		}
 	})
 
-	// c.OnHTML("table.table-list > tbody", func(h *colly.HTMLElement) {
-	// 	injunction := models.Injunction{}
-	// 	h.ForEach("tr", func(i int, h *colly.HTMLElement) {
-	// 		h.DOM.Find("td:nth-child(1)").Contents().Each(func(i int, s *goquery.Selection) {
-	// 			if i == 0 {
-	// 				number := strings.TrimSpace(s.Text())
-	// 				injunction.Number = number
-	// 			} else if i == 1 {
-	// 				rule := strings.TrimSpace(s.Find(".header").Text())
-	// 				description := strings.TrimSpace(s.Find(".description").Text())
-	// 				injunction.Rule = rule
-	// 				injunction.Description = description
-	// 			}
-	// 		})
+	c.OnHTML("table.table-list > tbody > tr:nth-child(1) > td:nth-child(2)", func(h *colly.HTMLElement) {
+		if !strings.Contains(currentUrl, "non-aktif") {
+			h.DOM.Contents().Each(func(i int, s *goquery.Selection) {
+				if i == 0 {
+					startDate := strings.TrimSpace(s.Text())
+					newExtraction.Metadata.StartDate = startDate
+				} else if i == 2 {
+					endDate := strings.TrimSpace(s.Text())
+					newExtraction.Metadata.EndDate = endDate
+				}
+			})
 
-	// 		h.DOM.Find("td:nth-child(2)").Contents().Each(func(i int, s *goquery.Selection) {
-	// 			if i == 0 {
-	// 				startDate := strings.TrimSpace(s.Text())
-	// 				injunction.StartDate = startDate
-	// 			} else if i == 2 {
-	// 				endDate := strings.TrimSpace(s.Text())
-	// 				injunction.EndDate = endDate
-	// 			}
-	// 		})
+			if strings.Contains(currentUrl, "penundaan") {
+				newExtraction.Metadata.Status = "pending"
+			} else if strings.Contains(currentUrl, "batal") {
+				newExtraction.Metadata.Status = "cancelled"
+			} else {
+				newExtraction.Metadata.Status = "active"
+			}
+		}
+	})
 
-	// 		injunction.PublishedDate = h.ChildText("tr:nth-child(1) > td:nth-child(3)")
-	// 	})
-	// 	injunctions = append(injunctions, injunction)
-	// })
+	c.OnHTML("table.table-list > tbody > tr:nth-child(1) > td:nth-child(3)", func(h *colly.HTMLElement) {
+		if !strings.Contains(currentUrl, "non-aktif") {
+			newExtraction.Metadata.PublishedDate = h.Text
+		}
+	})
+
+	c.OnHTML("table.ui.table.small.celled.very.padded tbody tr", func(e *colly.HTMLElement) {
+		if strings.Contains(currentUrl, "non-aktif") {
+			id := strings.Split(currentUrl, "#")
+			if strings.Contains(e.ChildAttr("a.button-detail", "data-id"), id[len(id)-1]) {
+				title := strings.TrimSpace(e.ChildText("td:nth-child(1) h5 a"))
+				number := strings.TrimSpace(e.ChildText("td:nth-child(1) div.npwp strong"))
+				city := strings.TrimSpace(e.ChildText("td:nth-child(2) .ui.list .item .content .header"))
+				address := strings.TrimSpace(e.ChildText("td:nth-child(2) .ui.list .item .content .description"))
+				endDate := strings.TrimSpace(e.ChildText("td:nth-child(3) table tbody tr:nth-child(2) td:nth-child(2)"))
+				startDate := strings.Split(strings.TrimSpace(e.ChildText("td:nth-child(3) table tbody tr:nth-child(3) td:nth-child(2)")), "s/d")
+
+				id := sha256.Sum256([]byte(title))
+				newExtraction.Metadata.ID = hex.EncodeToString(id[:])
+				newExtraction.Metadata.Title = title
+				newExtraction.Metadata.Number = number
+				newExtraction.Metadata.Address = address
+				newExtraction.Metadata.City = city
+				newExtraction.Metadata.EndDate = endDate
+				newExtraction.Metadata.StartDate = strings.TrimRight(startDate[0], "\n\r")
+				newExtraction.Metadata.EndDate = strings.Trim(startDate[1], "\n\r")
+				newExtraction.Metadata.Status = "not-active"
+			}
+		}
+	})
 
 	c.OnRequest(func(r *colly.Request) {
 		currentUrl = r.URL.String()
@@ -116,23 +181,22 @@ func buildScraper(queue *queue.Queue) (*colly.Collector, error) {
 		newExtraction.RawPageLink = null.StringFrom(rawPageUrl)
 		newExtraction.Id = hex.EncodeToString(frontierId[:])
 		newExtraction.UrlFrontierId = hex.EncodeToString(frontierId[:])
-		newExtraction.Metadata.Injunctions = injunctions
 
-		// fmt.Println("[started]: Upserting extraction", newExtraction.RawPageLink)
-		// err := scraper_service.UpsertExtraction(newExtraction)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	os.Exit(1)
-		// }
-		// fmt.Println("[finished] Upserting extraction", newExtraction.RawPageLink)
+		fmt.Println("[started]: Upserting extraction", newExtraction.RawPageLink)
+		err := scraper_service.UpsertExtraction(newExtraction)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("[finished] Upserting extraction", newExtraction.RawPageLink)
 
-		// fmt.Println("[started]: Upserting crawler", newExtraction.RawPageLink)
-		// err = services.UpdateUrlFrontierStatus(newExtraction.Id, crawler_model.URL_STATUS_CRAWLED)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	os.Exit(1)
-		// }
-		// fmt.Println("[finished] Upserting crawler", newExtraction.RawPageLink)
+		fmt.Println("[started]: Upserting crawler", newExtraction.RawPageLink)
+		err = services.UpdateUrlFrontierStatus(newExtraction.Id, crawler_model.URL_STATUS_CRAWLED)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("[finished] Upserting crawler", newExtraction.RawPageLink)
 	})
 
 	return c, nil
